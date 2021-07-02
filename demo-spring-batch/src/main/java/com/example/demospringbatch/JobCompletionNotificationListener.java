e8d3e73b9e376c1ac97996145383f3a7c7adbe34
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-07-02 19:54:04 -0300

    Adicionado @Component no arquivo pra buildar sem erro

--- demo-spring-batch/src/main/java/com/example/demospringbatch/JobCompletionNotificationListener.java
+++ demo-spring-batch/src/main/java/com/example/demospringbatch/JobCompletionNotificationListener.java
@@ -7,10 +7,12 @@ import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.listener.JobExecutionListenerSupport;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
 
 
 //O JobCompletionNotificationListener escuta quando o Job tem completado o batch Status
 // e usa o JdbcTemplate pra inspeccionar os resultados
+@Component
 public class JobCompletionNotificationListener extends JobExecutionListenerSupport {
 
     private static final Logger log = LoggerFactory.getLogger(JobCompletionNotificationListener.class);
